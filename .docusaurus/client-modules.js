0fb51074418cc599db28bd9dfe24bc026038b361
rollauda <rolland.auda@gmail.com>
2025-05-07 18:10:28 -0500

    up

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/rollandauda/Github/docusaurus/phil25/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/rollandauda/Github/docusaurus/phil25/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/rollandauda/Github/docusaurus/phil25/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/rollandauda/Github/docusaurus/phil25/src/css/custom.css"),
+];
