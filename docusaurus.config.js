6baf2a0351da2126d0ed6247e4d93d49003cb853
rollauda <rolland.auda@gmail.com>
2025-08-29 11:34:45 -0500

    up

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/philo.svg',
 
   // Set the production url of your site here
-  url: 'https://profauda.fr',
+  url: 'https://philo.profauda.fr',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/phil25/',
+  // For a site served at the root of a custom domain use '/'
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
